4c38bae3db15e5499cc0a96dd39e46838b8f0bfc
Martyn Masson <martyn.masson@gmail.com>
2019-03-19 16:13:04 +0000

    Mobile navigation looking good!

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,20 +1,25 @@
 (function() {
 	var navToggle = document.querySelector(".nav-toggle");
 	var mobileNav =  document.querySelector(".mobile-nav");
-	var body = document.querySelector("body");
 	var submenuToggle = document.querySelectorAll(".submenu-toggle");
 	var submenuToggleIcon = document.querySelector(".submenu-toggle i");
-	console.log(submenuToggleIcon);
 
-	body.addEventListener("keyup", (e) => {
+	document.addEventListener("keyup", (e) => {
 		if(e.keyCode == 27) {
-			mobileNav.classList.remove("mobile-nav--open")	;
+			mobileNav.classList.toggle("mobile-nav--open");
 		}
 	});
-
+	
 	navToggle.addEventListener("click", () => {
 		mobileNav.classList.toggle("mobile-nav--open");
 	});
+
+	window.addEventListener("mouseup", (e) => {
+		if(e.target != mobileNav && e.target.parentNode != mobileNav && e.target != submenuToggleIcon) {
+			mobileNav.classList.remove("mobile-nav--open");
+		}
+	});
+
 
 	for(var i = 0; i < submenuToggle.length; i++) {
 		submenuToggle[i].addEventListener("click", function() {
