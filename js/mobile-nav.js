6dcf04d582e4ec1f8c4ea8fc11cc8002f0d0cb3c
Martyn Masson <martyn.masson@gmail.com>
2019-03-19 12:22:00 +0000

    Navigation now closes if the escape key is pushed

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -3,6 +3,12 @@
 	var mobileNav =  document.querySelector(".mobile-nav");
 	var body = document.querySelector("body");
 
+	body.addEventListener("keyup", (e) => {
+		if(e.keyCode == 27) {
+			mobileNav.classList.remove("mobile-nav--open")	;
+		}
+	});
+
 	navToggle.addEventListener("click", () => {
 		mobileNav.classList.toggle("mobile-nav--open");
 	});
