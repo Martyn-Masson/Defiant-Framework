90b13a4090420e7d4a206944438547d02f196718
Martyn Masson <martyn.masson@gmail.com>
2019-04-07 09:30:05 +0100

    Fixed gallery bug

--- js/image-gallery.js
+++ js/image-gallery.js
@@ -2,20 +2,21 @@
 (function() {
 	var gallery = document.querySelector(".image-gallery");
 	var mainImage = document.querySelector(".image-gallery-main");
-	var images = document.querySelectorAll(".image-gallery img:not(:first-child)");
+	if(gallery) { var images = document.querySelectorAll(".image-gallery img:not(:first-child)"); }
 	var imageOpacity = .6;	
 
 	if(images) { images[0].style.opacity = imageOpacity; }
-
-	gallery.addEventListener("click", function(e) {
-		var target = e.target;
-
-		if(!target.classList.contains("image-gallery-main")) {
-			images.forEach(image => image.style.opacity = 1);
-			mainImage.src = target.src;
-			mainImage.classList.add("fade-in");
-			setTimeout(() => { mainImage.classList.remove("fade-in"); }, 500);
-			target.style.opacity = imageOpacity;
-		}
-	});
+	if(gallery) {
+		gallery.addEventListener("click", function(e) {
+			var target = e.target;
+	
+			if(!target.classList.contains("image-gallery-main")) {
+				images.forEach(image => image.style.opacity = 1);
+				mainImage.src = target.src;
+				mainImage.classList.add("fade-in");
+				setTimeout(() => { mainImage.classList.remove("fade-in"); }, 500);
+				target.style.opacity = imageOpacity;
+			}
+		});
+	}
 })();
